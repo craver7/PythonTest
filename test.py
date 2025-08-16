b50e7c11c980453642136a14c5a17bd0d3c8ed45
craver7 <craver7@gmail.com>
2025-08-16 18:50:14 +0900

    https://wikidocs.net/157866 1. 몬테카를 알고리즘 2. 유클리드 거리 공식

--- test.py
+++ test.py
@@ -64,3 +64,24 @@ my_list = [1, 2]
 num = 3
 my_list.extend([num] * num)
 print(my_list)
+
+import random
+
+count = 0    #부채꼴에 들어가는 점의 개수
+totalNum = 100000  #전체 점의 개수
+
+for i in range(totalNum):
+    x = random.uniform(0,1) #랜덤 x좌표
+    y = random.uniform(0,1) #랜덤 y좌표
+    if x**2 + y**2 <= 1:  #x**2 + y**2이 1보다 작거나 같으면
+        count += 1        #count를 증가시킴
+
+print(count / totalNum * 4)   # 부채꼴 안의 점의 개수 / 전체 점의 개수 * 4
+
+import math
+
+def distance_function(x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+
+print(distance_function(2,3,4,5))
